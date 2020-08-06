fbdf269438c7bd212998c10ed3c86480d981cd7b
Piyush696 <piyush.dutta@softobotics.com>
2020-08-06 21:21:49 +0530

    create case get all cases & get single case api

--- routes/cases.js
+++ routes/cases.js
@@ -2,18 +2,43 @@ const express = require('express');
 const router = express.Router();
 var passport = require('passport');
 const Case = require('../models').Case;
+const User = require('../models').User;
 
 /* create case. */
 router.post('/', passport.authenticate('jwt', { session: false }), function (req, res, next) {
-    Case.create({
-        countyOfArrest: req.body.countyOfArrest, dateOfArrest: req.body.dateOfArrest,
-        caseRelatedTo: req.body.caseRelatedTo, caseJurisdiction: req.body.caseJurisdiction,
-        nextCourtDate: req.body.nextCourtDate, legalRepresentative: req.body.legalRepresentation,
-        userId: req.user.userId
+    req.body['userId'] = req.user.userId
+    Case.create(req.body).then(data => {
+        res.json({ success: true, data: data });
+    })
+})
+
+
+/* get cases for user. */
+router.get('/', passport.authenticate('jwt', { session: false }), function (req, res, next) {
+    Case.findAll({
+        include: [
+            {
+                model: User,
+            }
+        ],
+        where: { userId: req.user.userId }
     }).then(data => {
         res.json({ success: true, data: data });
     })
 })
 
+//find case with caseId
+router.get('/case/:caseId', passport.authenticate('jwt', { session: false }), function (req, res, next) {
+    Case.findAll({
+        include: [
+            {
+                model: User,
+            }
+        ],
+        where: { caseId: req.params.caseId }
+    }).then(data => {
+        res.json({ success: true, data: data });
+    })
+})
 
 module.exports = router;
