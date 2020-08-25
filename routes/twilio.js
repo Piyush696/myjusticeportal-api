04f589c7a7aa72458a88eef16beb085287562d89
Piyush696 <piyush.dutta@softobotics.com>
2020-08-25 21:04:44 +0530

    api changes for email & username to userName twilio api

--- routes/twilio.js
+++ routes/twilio.js
@@ -16,8 +16,27 @@ function generateCode() {
     return Code;
 }
 
+/**generate otp during registration*/
+router.post('/', async function (req, res, next) {
+    let code = generateCode();
+    Twilio.findOne({ where: { twilioId: 1 } }).then(twilioCredentials => {
+        var client = new twilio(twilioCredentials.accountSid, twilioCredentials.authToken);
+        client.messages.create({
+            body: 'My Justice Portal' + ': ' + code + ' - This is your verification code.',
+            to: '+' + req.body.countryCode + req.body.mobile,  // Text this number
+            from: '+14048003419' // From a valid Twilio number
+        }).then((message) => {
+            User.update({ authCode: code, mobile: req.body.mobile, countryCode: req.body.countryCode }, { where: { userName: req.body.userName } }).then(() => {
+                res.json({ success: true })
+            }).catch(next)
+        }).catch((err) => {
+            res.json({ success: false })
+        })
+    })
+});
 
-/**generate otp */
+
+/**generate otp after registration*/
 router.post('/', passport.authenticate('jwt', { session: false }), async function (req, res, next) {
     let code = generateCode();
     Twilio.findOne({ where: { twilioId: 1 } }).then(twilioCredentials => {
@@ -50,6 +69,21 @@ router.post('/verify-sms', passport.authenticate('jwt', { session: false }), asy
         }
     }).catch(next)
 })
+
+/**verify otp */
+router.post('/register/verify-sms', async function (req, res, next) {
+    User.findOne({ where: { userName: req.body.userName } }).then((data) => {
+        let date = new Date();
+        let x = date - data.dataValues.updatedAt;
+        x = Math.round((x / 1000) / 60);
+        if (x <= 5 && data.dataValues.authCode == req.body.otp) {
+            res.json({ success: true, data: data })
+        } else {
+            res.json({ success: false, data: 'invalid otp' })
+        }
+    }).catch(next)
+})
+
 
 
 /* get twilio Credencials. */
