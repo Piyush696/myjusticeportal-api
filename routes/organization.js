b1f97e734634bfc9caaf18415c0be0ef12456784
Rajeshmaharana990 <59282611+Rajeshmaharana990@users.noreply.github.com>
2020-09-04 18:13:23 +0530

    org new folder

---
+++ routes/organization.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const uuidv1 = require('uuid/v1');
+const request = require('request');
+
+const Organization = require('../models').Organization;
+const Postage = require('../models').Postage;
+
+// To invite a user by mail.
+
+router.post('/invite-user', function (req, res, next) {
+    let url = req.headers.origin + "/" + req.body.facilityCode + "/registration/";
+    let uuid = uuidv1();
+    console.log("req.headers", req.headers);
+    console.log("url", url);
+    console.log("req.body", req.body);
+    Postage.findOne({ where: { postageAppId: 1 } }).then((postageDetails) => {
+        request.post({
+            headers: { 'content-type': 'application/json' },
+            url: `${postageDetails.dataValues.apiUrl}`,
+            json: {
+                "api_key": `${postageDetails.dataValues.apiKey}`,
+                "uid": `${uuid}`,
+                "arguments": {
+                    "recipients": [`${req.body.recipientEmail}`],
+                    "headers": {
+                        "subject": `${postageDetails.dataValues.project}` + ": New User Registration"
+                    },
+                    "template": "registration_invitation",
+                    "variables": {
+                        "name": `${req.user.firstName + ' ' + req.user.lastName}`,
+                        "invitationLink": `${url}`
+                    }
+                }
+            }
+        }, function (error, response) {
+            if ((response.body.response.status !== 'unauthorized') && (response.body.response.status != 'bad_request')) {
+                if (response.body.data.message.status == 'queued') {
+                    res.json({ success: true, data: 'Invitation sent' });
+                } else {
+                    res.json({ success: false, data: 'Invitation not sent' });
+                }
+            } else {
+                res.json({ success: false, data: 'Invitation not sent' });
+            }
+        });
+    }).catch((next) => {
+        console.log(next);
+    })
+});
+
+module.exports = router;
