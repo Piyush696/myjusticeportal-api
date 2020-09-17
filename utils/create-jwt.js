f4d794b0a7ad58a71f9b8c9a3976144d4f87f75a
Rajeshmaharana990 <59282611+Rajeshmaharana990@users.noreply.github.com>
2020-09-17 14:30:33 +0530

    isAdmin added in jwt

--- utils/create-jwt.js
+++ utils/create-jwt.js
@@ -3,12 +3,14 @@ const config = require('../config/config');
 
 module.exports = {
     createJwt: function (userData, rememberMe, callback) {
+        console.log(userData);
         let expiresIn = rememberMe ? '15d' : '2h';
         let token = jwt.sign({
             userId: userData.userId,
             firstName: userData.firstName,
             lastName: userData.lastName,
             userName: userData.userName,
+            isAdmin: userData.isAdmin,
             roles: userData.roles,
             facilities: userData.facilities,
             organizationId: userData.organizationId
