a8eed661b031a2828219a29a3f39820ac5e47338
Piyush696 <piyush.dutta@softobotics.com>
2020-09-21 12:06:12 +0530

    console removed and authentication added

--- routes/bondsman.js
+++ routes/bondsman.js
@@ -3,50 +3,57 @@ const router = express.Router();
 const User = require('../models').User;
 const Organization = require('../models').Organization;
 const Address = require('../models').Address;
-const Role = require('../models').Role
-const utils = require('../utils/file');
-const validateUtil = require('../utils/validateUser');
+const util = require('../utils/validateUser');
 
 //list of all organizations role is bondsman.
 router.get('/', function (req, res, next) {
-    console.log(req.user.userId)
-    User.findOne({
-        include: [
-            {
-                model: Organization, attributes: ['organizationId', 'name', 'orgCode', 'type'],
-                where: { type: 'bondsman' },
+    util.validate([6], req.user.roles, function (isAuthenticated) {
+        if (isAuthenticated) {
+            User.findOne({
                 include: [
                     {
-                        model: Address
+                        model: Organization, attributes: ['organizationId', 'name', 'orgCode', 'type'],
+                        where: { type: 'bondsman' },
+                        include: [
+                            {
+                                model: Address
+                            }
+                        ],
                     }
                 ],
-            }
-        ],
-        where: { userId: req.user.userId },
-        attributes: ['userId'],
-    }).then((user) => {
-        res.json({ success: true, data: user });
-    }).catch((next) => {
-        console.log(next)
+                where: { userId: req.user.userId },
+                attributes: ['userId'],
+            }).then((user) => {
+                res.json({ success: true, data: user });
+            }).catch(next)
+        } else {
+            res.json({ success: true, data: 'Unauthorized user.' });
+        }
     })
 })
 
 // get users of organisation.
 router.get('/:organizationId', function (req, res, next) {
-    Organization.findOne({
-        include: [
-            {
-                model: Address
-            },
-            {
-                model: User, attributes: ['userId', 'firstName', 'middleName', 'lastName', 'userName', 'createdAt']
-            }
-        ],
-        where: { organizationId: parseInt(req.params.organizationId) },
-        attributes: ['organizationId', 'name']
-    }).then(data => {
-        res.json({ success: true, data: data });
-    }).catch(next)
+    util.validate([6], req.user.roles, function (isAuthenticated) {
+        if (isAuthenticated) {
+            Organization.findOne({
+                include: [
+                    {
+                        model: Address
+                    },
+                    {
+                        model: User, attributes: ['userId', 'firstName', 'middleName', 'lastName', 'userName', 'createdAt']
+                    }
+                ],
+                where: { organizationId: parseInt(req.params.organizationId) },
+                attributes: ['organizationId', 'name']
+            }).then(data => {
+                res.json({ success: true, data: data });
+            }).catch(next)
+        } else {
+            res.json({ success: true, data: 'Unauthorized user.' });
+        }
+    })
 })
 
 module.exports = router;
