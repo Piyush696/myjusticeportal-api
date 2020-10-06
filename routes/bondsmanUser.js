dca422416999f1b1c68422cbc85f35672415f0e9
akshay-4362 <akshay.t@softobotics.com>
2020-10-06 21:42:28 +0530

    bondsman api changes

--- routes/bondsmanUser.js
+++ routes/bondsmanUser.js
@@ -8,10 +8,8 @@ const util = require('../utils/validateUser');
 router.post('/', function (req, res, next) {
     util.validate([1], req.user.roles, function (isAuthenticated) {
         if (isAuthenticated) {
-            req.body.map((element) => {
-                element['status'] = 'Requested',
-                    element['userId'] = req.user.userId
-            })
+            req.body['status'] = 'Requested',
+                req.body['userId'] = req.user.userId
             bondsman_user.create(req.body).then((bondrsmanUse) => {
                 res.json({ success: true, data: bondrsmanUse });
             }).catch((next) => {
@@ -28,15 +26,12 @@ router.post('/requested-users', function (req, res, next) {
     util.validate([6], req.user.roles, function (isAuthenticated) {
         if (isAuthenticated) {
             bondsman_user.findAll({
-                include: [
-                    {
-                        model: User,
-                    }
-                ],
-                where: { status: 'Requested', bondsman_userId: req.user.userId }
+                where: { status: 'Requested', bondsmanId: req.user.userId }
             }).then((foundConnections) => {
                 res.json({ success: true, data: foundConnections });
-            });
+            }).catch(next => {
+                console.log(next)
+            })
         }
         else {
             res.status(401).json({ success: false, data: 'User not authorized.' });
